a28e0d546e476997dc3ea7c231c4ef32986cc02c
Thais <thais.lopes@estudante.ufla.br>
2025-09-20 11:20:54 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,16 @@
+public class D {
+    private int D1;
+    private String D2;
+
+    // Getters
+    public int getD1() { return D1; }
+    public String getD2() { return D2; }
+
+    // Setters
+    public void setD1(int D1) { this.D1 = D1; }
+    public void setD2(String D2) { this.D2 = D2; }
+
+    // Métodos
+    public void MD1() { System.out.println("Executando MD1"); }
+    public void MD2() { System.out.println("Executando MD2"); }
+}
